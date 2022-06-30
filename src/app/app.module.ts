80970321897a43644566277bfb96716b74ceed1f
Sharron <sharronhemenway@outlook.com>
2022-06-30 11:29:12 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { InventoryInputComponent } from './components/inventory-input/inventory-input.component';
+import { InventoryListComponent } from './components/inventory-list/inventory-list.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    InventoryInputComponent,
+    InventoryListComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/inventory-input/inventory-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-input',
+  templateUrl: './inventory-input.component.html',
+  styleUrls: ['./inventory-input.component.css']
+})
+export class InventoryInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
